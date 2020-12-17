8d5ae5353dd248b7cbb612d7a3d1bc7ceb92225c
jrbeasle <jrbeasle@ncsu.edu>
2020-12-17 18:23:16 -0500

    MDLCORE-37: Remove item type "none" as an option from the grade category form (#183)
    Co-authored-by: Steve Bader <smbader@ncsu.edu>

--- grade/edit/tree/category_form.php
+++ grade/edit/tree/category_form.php
@@ -126,8 +126,7 @@ class edit_category_form extends moodleform {
             }
         }
 
-        $options = array(GRADE_TYPE_NONE=>get_string('typenone', 'grades'),
-                         GRADE_TYPE_VALUE=>get_string('typevalue', 'grades'),
+        $options = array(GRADE_TYPE_VALUE=>get_string('typevalue', 'grades'),
                          GRADE_TYPE_SCALE=>get_string('typescale', 'grades'),
                          GRADE_TYPE_TEXT=>get_string('typetext', 'grades'));
 
